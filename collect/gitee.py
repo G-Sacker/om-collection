e7091ebdbe8579c24489e08d25b06be5d428cec3
G-Sacker <ljx11714472@gmail.com>
2025-02-08 11:25:29 +0800

    fix Critical: "return" and "yield" should not be used in the same function.

--- collect/gitee.py
+++ collect/gitee.py
@@ -485,7 +485,7 @@ class GiteeClient():
 
         if response.status_code != 200:
             print("Gitee api get error: ", response.text)
-            return "Gitee api get error."
+            yield "Gitee api get error."
 
         items = response.text
 
